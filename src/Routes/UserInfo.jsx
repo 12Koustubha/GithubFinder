921f988dbe0292a2a1b3e3a7a8b3eef572f09c6a
mithiladk <mithila.deekay@gmail.com>
2023-05-10 11:44:49 +0530

    error fix

--- src/Routes/UserInfo.jsx
+++ src/Routes/UserInfo.jsx
@@ -1,4 +1,3 @@
-import { info } from "easyimage";
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Events from "../components/Events";
